05909e45b92d3959fa4184cb2af7d3a88482149f
Frederik Rabøl <frederik-rm@hotmail.com>
2015-08-17 09:35:16 +0200

    fix bugs, that was introduced at last commit where I used an old version of the repo.

--- wp-manual-cu.php
+++ wp-manual-cu.php
@@ -4,16 +4,48 @@ Plugin Name: WordPress Manual Content Units
 Plugin URI: https://github.com/BenjaminMedia/wp-manual-cu
 Description: Plugin that allows you to manually add Specific Content Units to WordPress
 Author: Simon Sessingø, Frederik Rabøl & Alf Henderson
-Version: 1.1.0
+Version: 1.1.1
 Author URI: http://www.bonnierpublications.com
 */
 
 $publicFolder = plugin_dir_url( __FILE__ ) . 'public';
-$postCount = 0;
 
 const HOOK_DEFAULT_MIDDLE = 'genesis_after_entry';
 const HOOK_DEFAULT_HORSESHOE = 'genesis_before_header';
+const HOOK_DEFAULT_STICKY = 'genesis_before_header';
 const HOOK_DEFAULT_FOOTER = 'wp_footer';
+
+// Enable shortcodes in widget-text
+add_filter('widget_text', 'do_shortcode');
+
+//[foobar]
+function insert_banner($attrs) {
+    $a = shortcode_atts( array(
+        'cu' => NULL,
+        'sticky' => FALSE,
+        'offset' => NULL,
+        'container' => ''
+    ), $attrs );
+
+    $cu = $a['cu'];
+    $offset = $a['offset'];
+    $isSticky = ($a['sticky']) ? ' data-listen="sticky-banner" data-offset="'.$offset.'" data-container="'. $a['container'] .'"' : '';
+
+    if(!is_null($cu)) {
+        $output = <<<HTML
+<div class="bonnier-banner-container">
+    <div class="banner"$isSticky>
+        <script type='text/javascript' src='http://eas4.emediate.eu/eas?cu=$cu;cre=mu;js=y;pageviewid=;target=_blank'></script>
+    </div>
+</div>
+HTML;
+        return $output;
+    }
+
+    return '';
+}
+add_shortcode('banner', 'insert_banner');
+
 
 // Add hooks
 add_action('admin_menu', function() {
@@ -23,6 +55,7 @@ add_action('admin_menu', function() {
 
 add_action(getOptionOrDefault('theme-hook-middle', HOOK_DEFAULT_MIDDLE), 'add_middle_banners');
 add_action(getOptionOrDefault('theme-hook-horseshoe', HOOK_DEFAULT_HORSESHOE), 'add_horseshoe_banners');
+add_action(getOptionOrDefault('theme-hook-sticky', HOOK_DEFAULT_STICKY), 'add_sticky_banners');
 add_action(getOptionOrDefault('theme-hook-footer', HOOK_DEFAULT_FOOTER), 'add_footer_banners');
 
 /**
@@ -73,7 +106,6 @@ function add_middle_banners() {
     $desktopMiddle = getOptionOrDefault('desktop-middle');
     $tabletMiddle = getOptionOrDefault('tablet-middle');
     $mobileMiddle = getOptionOrDefault('mobile-middle');
-    
 
     $output = <<<HTML
 <div class="bonnier-wrapper">
@@ -96,15 +128,33 @@ HTML;
     }
 }
 
-function add_horseshoe_banners() {
+function add_sticky_banners() {
+    $stickyLeft = getOptionOrDefault('sticky-left');
+    $stickyRight = getOptionOrDefault('sticky-right');
 
+    $output = <<<HTML
+<div class="bonnier-banner-container sticky">
+    <div class="left">
+        <div class="banner" data-listen="sticky-banner">
+            <script type='text/javascript' src='http://eas4.emediate.eu/eas?cu=$stickyLeft;cre=mu;js=y;pageviewid=;target=_blank'></script>
+        </div>
+    </div>
+    <div class="right">
+        <div class="banner" data-listen="sticky-banner">
+            <script type='text/javascript' src='http://eas4.emediate.eu/eas?cu=$stickyRight;cre=mu;js=y;pageviewid=;target=_blank'></script>
+        </div>
+    </div>
+</div>
+HTML;
+
+    echo $output;
+}
+
+function add_horseshoe_banners() {
     global $publicFolder;
 
     $sidebannerLeft = getOptionOrDefault('sidebanner-left');
     $sidebannerRight = getOptionOrDefault('sidebanner-right');
-
-    $stickyLeft = getOptionOrDefault('sticky-left');
-    $stickyRight = getOptionOrDefault('sticky-right');
 
     $desktopTop = getOptionOrDefault('desktop-top');
     $tabletTop = getOptionOrDefault('tablet-top');
@@ -112,22 +162,13 @@ function add_horseshoe_banners() {
 
     $output = <<<HTML
 <div class="bonnier-wrapper">
-    <div class="horseshoe" data-banner-horseshoe>
+     <div class="horseshoe">
         <div class="horseshoe-container">
             <div class="side-banner banner-left visible-md-lg" data-banner-md-lg>
                 <div id="EAS_fif_$sidebannerLeft"></div>
                 <script>
                     EAS_load_fif("EAS_fif_$sidebannerLeft", "$publicFolder/EAS_fif.html", "http://eas4.emediate.eu/eas?cu=$sidebannerLeft;cre=mu;js=y;pageviewid=;target=_blank", 240, 600);
                 </script>
-
-                <div class="static fixed" data-listen="sticky-banner">
-                    <div class="banner-min-height banner gtm-banner">
-                        <div id="EAS_fif_$stickyLeft"></div>
-                        <script>
-                            EAS_load_fif("EAS_fif_$stickyLeft", "$publicFolder/EAS_fif.html", "http://eas4.emediate.eu/eas?cu=$stickyLeft;cre=mu;js=y;pageviewid=;target=_blank", 320, 150);
-                        </script>
-                    </div>
-                </div>
             </div>
 
             <div class="top-banner" data-top-banner>
@@ -156,14 +197,6 @@ function add_horseshoe_banners() {
                 <script>
                     EAS_load_fif("EAS_fif_$sidebannerRight", "$publicFolder/EAS_fif.html", "http://eas4.emediate.eu/eas?cu=$sidebannerRight;cre=mu;js=y;pageviewid=;target=_blank", 240, 600);
                 </script>
-                <div class="static fixed" data-listen="sticky-banner">
-                    <div class="banner-min-height banner gtm-banner">
-                        <div id="EAS_fif_$stickyRight"></div>
-                        <script>
-                            EAS_load_fif("EAS_fif_$stickyRight", "$publicFolder/EAS_fif.html", "http://eas4.emediate.eu/eas?cu=$stickyRight;cre=mu;js=y;pageviewid=;target=_blank", 320, 150);
-                        </script>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
@@ -175,40 +208,41 @@ HTML;
 
 //settings page
 function mcu_settings_page() {
-	if (count($_POST) >= 1) {
-		echo"<div class='updated'> <p>Updated settings</p></div>";
+    if (count($_POST) >= 1) {
+        echo"<div class='updated'> <p>Updated settings</p></div>";
 
         foreach ($_POST as $key => $value) {
             if ($key != 'submit'){
                 update_option('wp-manual-cu-'.$key, $value, TRUE);
             }
         }
-	}
+    }
 
     $middleHook = getOptionOrDefault('theme-hook-middle', HOOK_DEFAULT_MIDDLE);
-    $stickyTopHook = getOptionOrDefault('theme-hook-sticky-top', HOOK_DEFAULT_HORSESHOE);
+    $horseshoeHook = getOptionOrDefault('theme-hook-horseshoe', HOOK_DEFAULT_HORSESHOE);
+    $stickyHook = getOptionOrDefault('theme-hook-sticky', HOOK_DEFAULT_STICKY);
     $footerHook = getOptionOrDefault('theme-hook-footer', HOOK_DEFAULT_FOOTER);
 
 
-	$desktopMiddle = get_option( 'wp-manual-cu-desktop-middle', NULL );
-	$tabletMiddle = get_option( 'wp-manual-cu-tablet-middle', NULL );
-	$mobileMiddle = get_option( 'wp-manual-cu-mobile-middle', NULL );
+    $desktopMiddle = getOptionOrDefault('desktop-middle', NULL);
+    $tabletMiddle = getOptionOrDefault('tablet-middle', NULL);
+    $mobileMiddle = getOptionOrDefault('mobile-middle', NULL);
 
-    $desktopTop = get_option( 'wp-manual-cu-desktop-top', NULL );
-    $tabletTop = get_option( 'wp-manual-cu-tablet-top', NULL );
-    $mobileTop = get_option( 'wp-manual-cu-mobile-top', NULL );
+    $desktopTop = getOptionOrDefault('desktop-top', NULL);
+    $tabletTop = getOptionOrDefault('tablet-top', NULL);
+    $mobileTop = getOptionOrDefault('mobile-top', NULL);
 
-    $sidebannerLeft = get_option( 'wp-manual-cu-sidebanner-left', NULL );
-    $sidebannerRight = get_option( 'wp-manual-cu-sidebanner-right', NULL );
+    $sidebannerLeft = getOptionOrDefault('sidebanner-left', NULL);
+    $sidebannerRight = getOptionOrDefault('sidebanner-right', NULL);
 
-    $desktopFooter = get_option( 'wp-manual-cu-desktop-footer', NULL );
-    $tabletFooter = get_option( 'wp-manual-cu-tablet-footer', NULL );
-    $mobileFooter = get_option( 'wp-manual-cu-mobile-footer', NULL );
+    $stickyLeft = getOptionOrDefault('sticky-left');
+    $stickyRight = getOptionOrDefault('sticky-right');
 
-    $stickyRight = get_option( 'wp-manual-cu-sticky-right', NULL );
-    $stickyLeft = get_option( 'wp-manual-cu-sticky-left', NULL );
+    $desktopFooter = getOptionOrDefault('desktop-footer', NULL);
+    $tabletFooter = getOptionOrDefault('tablet-footer', NULL);
+    $mobileFooter = getOptionOrDefault('mobile-footer', NULL);
 
-	wp_enqueue_style( 'AdminBootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css', array(), '', 'all' );
+    wp_enqueue_style( 'AdminBootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css', array(), '', 'all' );
 
     $form = <<< HTML
     <style>
@@ -216,85 +250,91 @@ function mcu_settings_page() {
         .padding-t { padding-top: 30px; }
         .padding-b { padding-bottom: 20px; }
     </style>
-	<div class="container">
-		<div class="row">
-		    <div class="col-xs-12">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
             <!--<h2>Manage Content Units</h2> -->
 
-			<form method="post" action=''>
-				<h3 class="padding-t">Horseshoe banners</h3>
+            <form method="post" action=''>
+                <h3 class="padding-t">Horseshoe banners</h3>
 
-				<p class="padding-b">
-			        Content units that will be displayed in the horseshoe (top of the page).
-			    </p>
+                <p class="padding-b">
+                    Content units that will be displayed in the horseshoe (top of the page).
+                </p>
 
                 <label for="theme-hook">Hook</label>
-				<input type="text" class="form-control form-group" placeholder="Hook for executing sticky/top banners" value="$stickyTopHook" name="theme-hook-sticky-top" />
+                <input type="text" class="form-control form-group" placeholder="Hook for outputting the horseshoe" value="$horseshoeHook" name="theme-hook-horseshoe" />
 
-				<label for="mobile-middle" class="padding-t">Desktop top</label>
-				<input type="text" class="form-control form-group" placeholder="Desktop" value="$desktopTop" name="desktop-top" />
+                <label for="mobile-middle" class="padding-t">Desktop top</label>
+                <input type="text" class="form-control form-group" placeholder="Desktop" value="$desktopTop" name="desktop-top" />
 
-				<label for="mobile-middle">Tablet top</label>
-				<input type="text" class="form-control form-group" placeholder="Tablet" value='$tabletTop' name="tablet-top" />
+                <label for="mobile-middle">Tablet top</label>
+                <input type="text" class="form-control form-group" placeholder="Tablet" value='$tabletTop' name="tablet-top" />
 
-				<label for='mobile-middle'>Mobile top</label>
-				<input type='text' class='form-control form-group' placeholder='Mobile' value='$mobileTop' name='mobile-top' />
+                <label for='mobile-middle'>Mobile top</label>
+                <input type='text' class='form-control form-group' placeholder='Mobile' value='$mobileTop' name='mobile-top' />
 
-
-				<label for='mobile-middle' class="padding-t">Left</label>
-				<input type='text' class='form-control form-group' placeholder='Sidebanner left' value='$sidebannerLeft' name='sidebanner-left' />
+                <label for='mobile-middle' class="padding-t">Left</label>
+                <input type='text' class='form-control form-group' placeholder='Sidebanner left' value='$sidebannerLeft' name='sidebanner-left' />
 
                 <label for='mobile-middle'>Right</label>
-				<input type='text' class='form-control form-group' placeholder='Sidebanner right' value='$sidebannerRight' name='sidebanner-right' />
+                <input type='text' class='form-control form-group' placeholder='Sidebanner right' value='$sidebannerRight' name='sidebanner-right' />
 
+                <h3 style="padding-top:30px;">Sticky banners</h3>
+                <p style="padding-bottom:20px;">
+                    Content units that will be displayed as sticky banners on each side of the page.
+                </p>
 
-                <label for='mobile-middle' class="padding-t">Sticky left</label>
-				<input type='text' class='form-control form-group' placeholder='Sticky left' value='$stickyLeft' name='sticky-left' />
+                <label for="theme-hook">Hook</label>
+                <input type="text" class="form-control form-group" placeholder="Hook for outputting the banners" value="$stickyHook" name="theme-hook-horseshoe" />
 
-				<label for='mobile-middle'>Sticky right</label>
-				<input type='text' class='form-control form-group' placeholder='Sticky right' value='$stickyRight' name='sticky-right' />
+                <label for="mobile-middle" class="padding-t">Left</label>
+                <input type="text" class="form-control form-group" placeholder="Desktop" value="$stickyLeft" name="sticky-left" />
+
+                <label for="mobile-middle">Right</label>
+                <input type="text" class="form-control form-group" placeholder="Tablet" value='$stickyRight' name="sticky-right" />
 
                 <h3 style="padding-top:30px;">Middle banners</h3>
-			    <p style="padding-bottom:20px;">
-			        Content units that will be displayed ever 3 posts.
-			    </p>
+                <p style="padding-bottom:20px;">
+                    Content units that will be displayed between each post.
+                </p>
 
-				<label for="theme-hook">Hook</label>
+                <label for="theme-hook">Hook</label>
 
-				<input type="text" class="form-control form-group" placeholder="Hook for executing middle bannerts" value="$middleHook" name="theme-hook-middle" />
+                <input type="text" class="form-control form-group" placeholder="Hook for executing middle bannerts" value="$middleHook" name="theme-hook-middle" />
 
-				<label for='dekstop-middle' class=padding-t>Desktop middle</label>
-				<input type='text' class='form-control form-group' placeholder='Desktop' value='$desktopMiddle' name='desktop-middle' />
-				<label for='tablet-middle'>Tablet middle</label>
-				<input type='text' class='form-control form-group' placeholder='Tablet' value='$tabletMiddle' name='tablet-middle' />
-				<label for='mobile-middle'>Mobile middle</label>
-				<input type='text' class='form-control form-group' placeholder='Mobile' value='$mobileMiddle' name='mobile-middle' />
+                <label for='dekstop-middle' class=padding-t>Desktop middle</label>
+                <input type='text' class='form-control form-group' placeholder='Desktop' value='$desktopMiddle' name='desktop-middle' />
+                <label for='tablet-middle'>Tablet middle</label>
+                <input type='text' class='form-control form-group' placeholder='Tablet' value='$tabletMiddle' name='tablet-middle' />
+                <label for='mobile-middle'>Mobile middle</label>
+                <input type='text' class='form-control form-group' placeholder='Mobile' value='$mobileMiddle' name='mobile-middle' />
 
                 <h3 class="padding-t">Footer banners</h3>
 
-				<p class="padding-b">
-			        Content units that will be displayed in the footer
-			    </p>
+                <p class="padding-b">
+                    Content units that will be displayed in the footer
+                </p>
 
-			    <label for="theme-hook">Hook</label>
-				<input type="text" class="form-control form-group" placeholder="Hook for executing footer banners" value="$footerHook" name="theme-hook-footer" />
+                <label for="theme-hook">Hook</label>
+                <input type="text" class="form-control form-group" placeholder="Hook for executing footer banners" value="$footerHook" name="theme-hook-footer" />
 
                 <label for="mobile-middle" class="padding-t">Desktop</label>
-				<input type='text' class='form-control form-group' placeholder='Desktop' value='$desktopFooter' name='desktop-footer' />
+                <input type='text' class='form-control form-group' placeholder='Desktop' value='$desktopFooter' name='desktop-footer' />
 
-				<label for="mobile-middle">Tablet</label>
-				<input type='text' class="form-control form-group" placeholder="Tablet" value="$tabletFooter" name="tablet-footer" />
+                <label for="mobile-middle">Tablet</label>
+                <input type='text' class="form-control form-group" placeholder="Tablet" value="$tabletFooter" name="tablet-footer" />
 
-				<label for="mobile-middle">Mobile top</label>
-				<input type="text" class="form-control form-group" placeholder="Mobile" value="$mobileFooter" name="mobile-footer" />
+                <label for="mobile-middle">Mobile top</label>
+                <input type="text" class="form-control form-group" placeholder="Mobile" value="$mobileFooter" name="mobile-footer" />
 
 
-				<input type='submit' name='submit' value='Save' class='btn btn-primary' style="margin-top: 30px;" />
-			</form>
-			</div>
-		</div>
-	</div>
+                <input type='submit' name='submit' value='Save' class='btn btn-primary' style="margin-top: 30px;" />
+            </form>
+            </div>
+        </div>
+    </div>
 HTML;
 
-	echo $form;
+    echo $form;
 }
